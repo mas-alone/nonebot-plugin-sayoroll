08412ce092d325e9aef2dd540390de2964eb69fa
A M D <52590027+mas-alone@users.noreply.github.com>
2023-12-25 13:12:39 +0800

    Update __init__.py

--- nonebot_plugin_sayoroll/__init__.py
+++ nonebot_plugin_sayoroll/__init__.py
@@ -36,7 +36,7 @@ async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
     args = str(args).strip()
 
     if not args:
-        msg = '你的数字是[{}]'.format(random.randint(0, 100))
+        msg = '{}'.format(random.randint(0, 100))
         await roll.finish(
             message=MessageSegment.reply(event.message_id) + Message(msg)
         )
@@ -46,13 +46,13 @@ async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
         if num > 99999:
             msg = '数字太大了，你真的需要这么大的随机数吗？'
         else:
-            msg = '你的数字是[{}]'.format(random.randint(0, num))
+            msg = '{}'.format(random.randint(0, num))
         await roll.finish(
             message=MessageSegment.reply(event.message_id) + Message(msg)
         )
 
-    elif re.search(r'([\u4e00-\u9fff])([\u4e00-\u9fff])\1(.*?)', args):
-        result = re.search(r'([\u4e00-\u9fff])([\u4e00-\u9fff])\1(.*?)', args)
+    elif re.search(r'([\u4e00-\u9fff])[不没].*?\1(.*?)', args):
+        result = re.search(r'([\u4e00-\u9fff])[不没].*?\1(.*?)', args)
         options = [result.group()[:1], result.group()[1:]]
         if args.startswith('你'):
             msg = args[:result.span()[0]].replace('我', 'temp').replace('你', '我').replace('temp', '你') + random.choice(options) + args[result.span()[1]:]
@@ -94,7 +94,7 @@ async def _(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
 
         if any(args_without_punctuation.split(' ').count(x) >= 2 for x in set(args_without_punctuation.split(' '))):
             duplicate_options = [x for x in set(args_without_punctuation.split(' ')) if args_without_punctuation.split(' ').count(x) >= 2]
-            msg = '[{}] 参数出现次数过多,想增大概率是吧'.format(','.join(duplicate_options))
+            msg = '[{}] 出现次数过多,想增大概率是吧'.format(','.join(duplicate_options))
             await roll.finish(
                 message=MessageSegment.reply(event.message_id) + Message(msg)
             )
